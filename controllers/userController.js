cfcdc4df52a934273fdafb7878d666fa1b9f810a
John Kuefler <john@dev-squared.com>
2020-10-25 14:41:54 -0500

    working on users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,75 @@
+const User = require('../models/user');
+
 exports.get_index = function(req, res) {
-  res.render('settings/users/index');
+  User.find({}, function(err, users) {
+    if (err) {
+      // handle error
+    } else {
+      res.render('settings/users/index', {data: users});
+    }
+  });
 };
 
+exports.get_create = function(req, res) {
+  res.render('settings/users/create', {message: req.flash('signupMessage')});
+};
+
+exports.post_create = function(req, res) {
+  const newUser = new User();
+
+  // set the user's local credentials
+  newUser.email = req.body.email;
+  newUser.password = newUser.generateHash(req.body.password);
+  newUser.firstName = req.body.firstName;
+  newUser.lastName = req.body.lastName;
+  newUser.role = req.body.role;
+
+  // save the user
+  newUser.save(function(err) {
+    if (err) {
+      throw err;
+    }
+    res.redirect('/settings/users');
+  });
+};
+
+exports.get_update = function(req, res) {
+  User.findOne({_id: req.query.id}, function(err, user) {
+    if (err) {
+      // handle error
+    } else {
+      res.render('settings/users/update', {data: user, message: req.flash('signupMessage')});
+    }
+  });
+};
+
+exports.post_update = function(req, res) {
+  const updateData = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
+    role: req.body.role,
+  };
+  if (req.body.password) {
+    updateData.password = User.generateHash(req.body.password);
+  }
+
+  User.findOneAndUpdate({_id: req.body.id}, updateData, function(err, data) {
+    if (err) {
+      // handle error
+      console.log(err);
+    } else {
+      res.redirect('/settings/users');
+    }
+  });
+};
+
+exports.get_delete = function(req, res) {
+  User.findOneAndDelete({_id: req.query.id}, function(err) {
+    if (err) {
+      // handle error
+      console.log(err);
+    } else {
+      res.redirect('/settings/users');
+    }
+  });
+};
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,18 @@ const router = express.Router();
 const usersController = require('../controllers/userController');
 const authMiddleware = require('../middleware/ensureAuthenticated');
 
+
 router.get('/', authMiddleware.ensureAuthenticated, usersController.get_index);
+
+router.get('/create', authMiddleware.ensureAuthenticated, usersController.get_create);
+
+router.post('/create', authMiddleware.ensureAuthenticated, usersController.post_create);
+
+router.get('/update', authMiddleware.ensureAuthenticated, usersController.get_update);
+
+router.post('/update', authMiddleware.ensureAuthenticated, usersController.post_update);
+
+router.get('/delete', authMiddleware.ensureAuthenticated, usersController.get_delete);
 
 module.exports = router;
 
